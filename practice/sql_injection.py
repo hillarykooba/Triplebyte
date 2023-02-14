133faf5fdd7a8458e5ef0d30b7bc76130196d4f0
Hillary Kooba <koobasam@gmail.com>
2023-02-15 00:15:04 +0300

    Update sql_injection.py

--- practice/sql_injection.py
+++ practice/sql_injection.py
@@ -6,7 +6,7 @@ SELECT * FROM users WHERE email = 'admin@example.com'
 This would bypass the password verification and let the attacker login as the admin. The key part of the attack is the single quote after "password", which allows the attacker to break out of the string comparison and modify the intent of the query. One solution here is to sanitize or escape user inputs instead of directly interpolating them into SQL statements. However this can be fragile and error-prone, so the best practice is generally to use prepared statements, which lets the database itself protect against SQL injections.
 '''
 
-The following code seems to work, but it wouldn't pass a security review. Which vulnerability is it exposed to?
+# The following code seems to work, but it wouldn't pass a security review. Which vulnerability is it exposed to?
 
 def login(request, cursor):
     cursor.execute("SELECT * FROM users WHERE email = '" \
